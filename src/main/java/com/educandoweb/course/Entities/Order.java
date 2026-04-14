0656b2f84b580c0d93cff897ad053a32aa7812d7
J Bruno <jbrunorodrigues13@gmail.com>
2026-04-14 09:05:09 -0300

    Implements OrderStatus enum

--- src/main/java/com/educandoweb/course/Entities/Order.java
+++ src/main/java/com/educandoweb/course/Entities/Order.java
@@ -1,5 +1,6 @@
 package com.educandoweb.course.Entities;
 
+import com.educandoweb.course.Entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 
@@ -16,15 +17,18 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
 
     public Order() {}
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -44,6 +48,16 @@ public class Order implements Serializable {
     public Order setMoment(Instant moment) {
         this.moment = moment;
         return this;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     public User getClient() {
